4349e6605207203dde6f9563cea7efc87083fa4c
Kristian Fougner <kristian.fougner@nhn.no>
2024-08-05 16:00:11 +0200

    fiks!

--- src/app/child-component/child-component.component.ts
+++ src/app/child-component/child-component.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-child-component',
+  selector: '[childComponent]',
   templateUrl: './child-component.component.html',
   styleUrls: ['./child-component.component.css']
 })
